47f8e67e3aa712d2f6b1277310eeb413b8b0a46a
Devon <devoncoates27@gmail.com>
2021-02-04 23:16:47 -0800

    Lives Counter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //Ceiling Variables
+    public bool ceiling;
+    public LayerMask whatIsCei;
+    public Transform ceiChecker;
+    public float ceiCheckerRad;
+
+    //Block Variables
+    public bool sprung;
+    public LayerMask whatIsSpr;
+    public bool teleport;
+    public LayerMask whatIsTel;
     public float speed;
     private bool canMove;
     private Rigidbody2D theRB2D;
@@ -21,9 +32,12 @@ public class PlayerController : MonoBehaviour
     private Animator theAnimator;
 
     public GameManager theGM;
+    private LivesManager theLM;
 
     void Start()
     {
+        theLM = FindObjectOfType<LivesManager>();
+
         theRB2D = GetComponent<Rigidbody2D>();
         theAnimator = GetComponent<Animator>();
 
@@ -33,16 +47,29 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        if ((Input.GetAxisRaw("Horizontal") > 0.5f && !ceiling) || (Input.GetAxisRaw("Horizontal") < -0.5f && !ceiling))
         {
             canMove = true;
         }
+        if (sprung == true)
+        {
+            theRB2D.velocity = new Vector2(theRB2D.velocity.x, 50);
+        }
+        if (teleport == true)
+        {
+            Vector2 tpPosition = new Vector2(-7.67f, 1.5f);
+            theRB2D.MovePosition(tpPosition);
+        }
+
 
     }
 
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(grdChecker.position, grdCheckerRad, whatIsGrd);
+        sprung = Physics2D.OverlapCircle(grdChecker.position, grdCheckerRad, whatIsSpr);
+        teleport = Physics2D.OverlapCircle(grdChecker.position, grdCheckerRad, whatIsTel);
+        ceiling = Physics2D.OverlapCircle(ceiChecker.position, ceiCheckerRad, whatIsCei);
 
 
         MovePlayer();
@@ -68,6 +95,23 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector2(-1f, 1f);
             }
         }
+        if (ceiling)
+        {
+            theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, theRB2D.velocity.y);
+
+            theAnimator.SetFloat("Speed", Mathf.Abs(theRB2D.velocity.x));
+
+            Physics2D.gravity = new Vector2(0, 0);
+
+            if (theRB2D.velocity.x > 0)
+            {
+                transform.localScale = new Vector2(1f, -1f);
+            }
+            else if (theRB2D.velocity.x < 0)
+            {
+                transform.localScale = new Vector2(-1f, -1f);
+            }
+        }
     }
 
     void Jump()
@@ -77,6 +121,14 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 theRB2D.velocity = new Vector2(theRB2D.velocity.x, jumpForce);
+            }
+        }
+        if (ceiling == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                theRB2D.velocity = new Vector2(theRB2D.velocity.x, -jumpForce);
+                Physics2D.gravity = new Vector2(0, -9.8f);
             }
         }
 
@@ -109,7 +161,8 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "Spike")
         {
             Debug.Log("Ouch!");
-            theGM.GameOver();
+            //theGM.GameOver();
+            theLM.TakeLife();
         }
     }
 }
