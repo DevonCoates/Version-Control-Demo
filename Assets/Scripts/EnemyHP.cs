66847841eab77a3f1eb6b54f40afd275ec8243a9
Devon <devoncoates27@gmail.com>
2021-03-07 21:06:41 -0800

    DeathAnim

--- Assets/Scripts/EnemyHP.cs
+++ Assets/Scripts/EnemyHP.cs
@@ -6,11 +6,22 @@ public class EnemyHP : MonoBehaviour
 {
     public int enemyHP;
     private int currentHP;
+
+    private Animator theAnim;
+    private bool isDead;
+
+    private Collider2D parentCol;
+    private Collider2D hurtboxCol;
     
     // Start is called before the first frame update
     void Start()
     {
         currentHP = enemyHP;
+
+        theAnim = transform.parent.GetComponent<Animator>();
+
+        parentCol = transform.parent.GetComponent<Collider2D>();
+        hurtboxCol = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -18,12 +29,22 @@ public class EnemyHP : MonoBehaviour
     {
         if(currentHP <= 0)
         {
-            Destroy(transform.parent.gameObject);
+            isDead = true;
+            theAnim.SetBool("Dead", isDead);
+            parentCol.enabled = false;
+            hurtboxCol.enabled = false;
+            StartCoroutine("KillSwitch");
         }
     }
 
     public void TakeDamage(int damage)
     {
         currentHP -= damage;
+    }
+
+    IEnumerator KillSwitch()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(transform.parent.gameObject);
     }
 }
